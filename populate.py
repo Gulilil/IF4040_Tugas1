936d7b90a464bc8d5a4c28d61d9dcb8085ff22c0
Gulilil <juan.csantoso@gmail.com>
2024-10-09 12:29:54 +0700

    finishing seeding

--- populate.py
+++ populate.py
@@ -5,11 +5,14 @@ import random
 from multiprocessing import Process
 from names_generator import generate_name
 import time
+from random_address import real_random_address
 
 DATA_CSV_DIR = os.path.join(os.getcwd(), "data_csv")
 DATA_FINAL_DIR = os.path.join(os.getcwd(), "data_final")
 DATA_TEMP_DIR = os.path.join(os.getcwd(), "data_temp")
 LIMIT = 500000
+LIMIT_MORE = 2000000
+LIMIT_MORE_MORE = 5000000
 COMPANIES_LAST_ID = 1125
 GROUPS_LAST_ID = 447
 COUNTRIES_LAST_ID = 195
@@ -100,6 +103,7 @@ def generate_idols_data(df_countries: pd.DataFrame,cols: list, count: int, offse
   for i in range(1, count+1):
     # ID
     id = offset + i
+    
     # Full Name
     name = generate_name(style='capital')
     names = name.split(" ")
@@ -255,6 +259,115 @@ def generate_songs_data(cols: list, count: int, offset: int, process: int = 1):
   filename = os.path.join(DATA_TEMP_DIR, f"songs_P{process}.csv")
   result_df.to_csv(filename, index=False)
 
+def generate_customers_data(cols: list, count: int, offset: int, process: int = 1):
+  result_data = []
+  checkpoint_count = count // 10
+  for i in range(1, count+1):
+    # ID
+    id = offset + i
+
+    # Album_id
+    album_id = random.randint(1, LIMIT)
+
+    # Full Name
+    name = generate_name(style='capital')
+    names = name.split(" ")
+    f_name = names[0]
+    l_name = names[1]
+
+    rand_int = random.randint(0, 1)
+    if (rand_int == 0):
+      res_name = f"{f_name} {l_name}"
+      # Email
+      email = f"{f_name}.{l_name}@email.com"
+
+    else:
+      res_name = f"{l_name} {f_name}"
+      # Email
+      email = f"{l_name}.{f_name}@email.com"
+
+    # Username
+    rand_int = random.randint(0, 1)
+    if rand_int == 0 : username = f"{f_name[:len(f_name)//2]}{l_name[-len(l_name)//2:]}" 
+    else: username = f"{l_name[:len(l_name)//2]}{f_name[-len(f_name)//2:]}" 
+
+    # Password
+    password = "password"
+
+    # Address 
+    address = real_random_address()
+
+
+    # Country_id
+    country_id = random.randint(1, COUNTRIES_LAST_ID)
+
+    row_data = [id, res_name, email, username, password, address, country_id]
+    result_data.append(row_data)
+
+    if (i % checkpoint_count == 0):
+      print(f"[CHECKPOINT] Generating Customers data checkpoint {i} data on P{process}")
+  
+  result_df = pd.DataFrame(result_data, columns=cols)
+  filename = os.path.join(DATA_TEMP_DIR, f"customers_P{process}.csv")
+  result_df.to_csv(filename, index=False)
+
+
+def generate_transactions_data(cols: list, count: int, offset: int, process: int = 1):
+  result_data = []
+  checkpoint_count = count // 10
+  for i in range(1, count+1):
+    # ID
+    id = offset + i
+
+    # Customer ID
+    customer_id = random.randint(1, LIMIT)
+
+    # Full Name
+    year = random.randint(2010, 2023)
+    month = random.randint(1, 12)
+    date = random.randint(1, 29)
+    hour = random.randint(0, 23)
+    minute = random.randint(0,59)
+    second = random.randint(0,59)
+    time_string = f'{year}-{month}-{date} {hour}:{minute}:{second}'
+
+    # Status
+    status_list = ['paid', 'cancelled', 'pending']
+    status = status_list[random.randint(0, len(status_list)-1)]
+
+    row_data = [id, customer_id, time_string, status]
+    result_data.append(row_data)
+
+    if (i % checkpoint_count == 0):
+      print(f"[CHECKPOINT] Generating Transactions data checkpoint {i} data on P{process}")
+  
+  result_df = pd.DataFrame(result_data, columns=cols)
+  filename = os.path.join(DATA_TEMP_DIR, f"transactions_P{process}.csv")
+  result_df.to_csv(filename, index=False)
+
+def generate_transaction_albums_data(cols: list, count: int, offset: int, process: int = 1):
+  result_data = []
+  checkpoint_count = count // 10
+  for i in range(1, count+1):
+    # ID
+    trans_id = random.randint(1, LIMIT_MORE)
+
+    # Customer ID
+    customer_id = random.randint(1, LIMIT)
+
+    # Quantity
+    quantity = random.randint(1, 10)
+
+    row_data = [trans_id, customer_id, quantity]
+    result_data.append(row_data)
+
+    if (i % checkpoint_count == 0):
+      print(f"[CHECKPOINT] Generating Transaction_Albums data checkpoint {i} data on P{process}")
+  
+  result_df = pd.DataFrame(result_data, columns=cols)
+  filename = os.path.join(DATA_TEMP_DIR, f"transaction_albums_P{process}.csv")
+  result_df.to_csv(filename, index=False)
+
 
 if __name__ == "__main__":
   files = []
@@ -266,14 +379,14 @@ if __name__ == "__main__":
 
 
   # Reading each file
-  for file in filename:
-    file_path = os.path.join(DATA_CSV_DIR, file)
+  # for file in filename:
+  #   file_path = os.path.join(DATA_CSV_DIR, file)
 
-    # Read df
-    amount = int(LIMIT // 4)
-    df = pd.read_csv(file_path)
-    cols = df.columns.tolist()
-    offset = df['id'].max()
+  #   # Read df
+  #   amount = int(LIMIT // 4)
+  #   df = pd.read_csv(file_path)
+  #   cols = df.columns.tolist()
+  #   offset = df['id'].max()
 
 
     # if (file == 'companies.csv'):
@@ -296,11 +409,11 @@ if __name__ == "__main__":
     #   p3 = Process(target=generate_idols_data, args=(df_countries, cols, amount, offset + 2*amount, 3))
     #   p4 = Process(target=generate_idols_data, args=(df_countries, cols, amount, offset + 3*amount, 4))
 
-    if (file == 'albums.csv'):
-      p1 = Process(target=generate_albums_data, args=(cols, amount, offset, 1))
-      p2 = Process(target=generate_albums_data, args=(cols, amount, offset + amount, 2))
-      p3 = Process(target=generate_albums_data, args=(cols, amount, offset + 2*amount, 3))
-      p4 = Process(target=generate_albums_data, args=(cols, amount, offset + 3*amount, 4))
+    # if (file == 'albums.csv'):
+    #   p1 = Process(target=generate_albums_data, args=(cols, amount, offset, 1))
+    #   p2 = Process(target=generate_albums_data, args=(cols, amount, offset + amount, 2))
+    #   p3 = Process(target=generate_albums_data, args=(cols, amount, offset + 2*amount, 3))
+    #   p4 = Process(target=generate_albums_data, args=(cols, amount, offset + 3*amount, 4))
 
     # if (file == 'songs.csv'):
     #   p1 = Process(target=generate_songs_data, args=(cols, amount, offset, 1))
@@ -309,15 +422,55 @@ if __name__ == "__main__":
     #   p4 = Process(target=generate_songs_data, args=(cols, amount, offset + 3*amount, 4))
 
 
-      p1.start()
-      p2.start()
-      p3.start()
-      p4.start()
+    # p1.start()
+    # p2.start()
+    # p3.start()
+    # p4.start()
 
-      p1.join()
-      p2.join()
-      p3.join()
-      p4.join()
+    # p1.join()
+    # p2.join()
+    # p3.join()
+    # p4.join()
+
+
+  # =========================================================
+  # For data that do not have the initial csv
+  amount = int(LIMIT // 4)
+  amount_more = int(LIMIT_MORE//4)
+  amount_more_more = int(LIMIT_MORE_MORE//4)
+  customers_cols = ['id', 'name', 'email', 'username', 'password', 'address', 'country_id']
+  transactions_cols = ['id', 'customer_id', 'created_on', 'status']
+  transaction_albums_cols = ['transaction_id', 'customer_id', 'quantity']
+  offset = 0
+
+  # # Customers
+  # p1 = Process(target=generate_customers_data, args=(customers_cols, amount, offset, 1))
+  # p2 = Process(target=generate_customers_data, args=(customers_cols, amount, offset + amount, 2))
+  # p3 = Process(target=generate_customers_data, args=(customers_cols, amount, offset + 2*amount, 3))
+  # p4 = Process(target=generate_customers_data, args=(customers_cols, amount, offset + 3*amount, 4))
+
+  # # Transactions
+  # p1 = Process(target=generate_transactions_data, args=(transactions_cols, amount_more, offset, 1))
+  # p2 = Process(target=generate_transactions_data, args=(transactions_cols, amount_more, offset + amount_more, 2))
+  # p3 = Process(target=generate_transactions_data, args=(transactions_cols, amount_more, offset + 2*amount_more, 3))
+  # p4 = Process(target=generate_transactions_data, args=(transactions_cols, amount_more, offset + 3*amount_more, 4))
+
+  # # TransactionAlbums
+  # p1 = Process(target=generate_transaction_albums_data, args=(transaction_albums_cols, amount_more_more, offset, 1))
+  # p2 = Process(target=generate_transaction_albums_data, args=(transaction_albums_cols, amount_more_more, offset + amount_more, 2))
+  # p3 = Process(target=generate_transaction_albums_data, args=(transaction_albums_cols, amount_more_more, offset + 2*amount_more, 3))
+  # p4 = Process(target=generate_transaction_albums_data, args=(transaction_albums_cols, amount_more_more, offset + 3*amount_more, 4))
+
+  # p1.start()
+  # p2.start()
+  # p3.start()
+  # p4.start()
+
+  # p1.join()
+  # p2.join()
+  # p3.join()
+  # p4.join()
+
 
   # End time
   end = time.time()
